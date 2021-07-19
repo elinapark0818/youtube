f3b51f28b26697f1451c256e43dc7f2bcee5ca11
Elina <elinapark0818@gmail.com>
2021-07-20 00:49:08 +0900

    vidoeList, videoItem 설정

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,29 @@
+import React, {useEffect, useState} from 'react';
 import './app.css';
+import VideoList from './components/videoList';
+
 
 function App() {
-  return (
-    <h1>Hello :)</h1>
-  );
+  const apiKey = process.env.REACT_APP_API_KEY;
+  const [videos, setVideos] = useState([]);
+  
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=25&key=${apiKey}`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos}/>;
+
 }
 
 export default App;
