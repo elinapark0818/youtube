5c39667905bdf89813ed03b156fe12fabc2b91c5
Elina <elinapark0818@gmail.com>
2021-07-21 19:53:57 +0900

    강의들으면서 클론코딩

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,26 @@
 import React, {useEffect, useState} from 'react';
 import './app.css';
-import VideoList from './components/videoList';
+import VideoList from './components/video_list/videoList';
 
 
 function App() {
-  const apiKey = process.env.REACT_APP_API_KEY;
+  // const apiKey = process.env.REACT_APP_API_KEY;
   const [videos, setVideos] = useState([]);
   
   useEffect(() => {
+    
     const requestOptions = {
       method: 'GET',
-      redirect: 'follow',
+      redirect: 'follow'
     };
-
-    fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=KR&maxResults=25&key=${apiKey}`,
-      requestOptions
+    
+    fetch("https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyBBdCszbsbD9nIHn-jrMS_0mLYTIXuUcLQ&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBBdCszbsbD9nIHn-jrMS_0mLYTIXuUcLQ", 
+    requestOptions
     )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+
   }, []);
 
   return <VideoList videos={videos}/>;
