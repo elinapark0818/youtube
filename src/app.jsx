486086091b20feed5d150bc0b7c74546b30e688b
Elina <elinapark0818@gmail.com>
2021-07-22 22:03:02 +0900

    Add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,28 @@
 import React, {useEffect, useState} from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/searchHeader/searchHeader';
 import VideoList from './components/video_list/videoList';
 
 // const apiKey = process.env.REACT_APP_API_KEY;
 
-function App() {
+function App({ youtube }) {
+  
   const [videos, setVideos] = useState([]);
+  const search = query => {
+    youtube.search(query)
+    .then(videos => setVideos(videos))
+  };  
   
   useEffect(() => {
-    
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://youtube.googleapis.com/youtube/v3/videos?key=AIzaSyBBdCszbsbD9nIHn-jrMS_0mLYTIXuUcLQ&part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBBdCszbsbD9nIHn-jrMS_0mLYTIXuUcLQ", 
-    requestOptions
-    )
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
-
+    youtube.mostPopular()
+    .then(videos => setVideos(videos))
   }, []);
 
-  return <VideoList videos={videos}/>;
-
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
-
 export default App;
